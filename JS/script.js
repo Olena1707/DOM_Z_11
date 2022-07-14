5a266f98c0ccb69ac99bb0441868058b6f26e6e0
Elena <ukr.7905432@gmail.com>
2022-07-14 13:08:44 -0600

    load complimentation

---
+++ JS/script.js
@@ -0,0 +1,56 @@
+"use strict";
+
+/*let a = prompt("Введите число");*/
+
+/*let resalt = a == 0 ? "Верно" : "Неверно";
+alert(resalt);*/
+
+/*let resalt = a > 0 ? "Верно" : "Неверно";
+alert(resalt);*/
+
+/*let resalt = a < 0 ? "Верно" : "Неверно";
+alert(resalt);*/
+
+/*let resalt = a >= 0 ? "Верно" : "Неверно";
+alert(resalt);*/
+
+/*let resalt = a <= 0 ? "Верно" : "Неверно";
+alert(resalt);*/
+
+/*let resalt = a != 0 ? "Верно" : "Неверно";
+alert(resalt);*/
+
+/*let resalt = a === "test" ? "Верно" : "Неверно";
+alert(resalt);*/
+
+/*let resalt = a === "1" ? "Верно" : "Неверно";
+alert(resalt);*/
+
+/*let resalt = a > 0 && a < 5 ? "Верно" : "Неверно";
+alert(resalt);*/
+
+/*let resalt = a == 0 || a == 2 ? +a + +7 : a / 10;
+alert(resalt);*/
+
+/*let b = prompt("Введите еще одно число");
+let resalt = a <= 1 && b >= 3 ? +a + +b : a - b;
+alert(resalt);*/
+
+/*let b = prompt("Введите еще одно число");
+let resalt = (a > 2 && a < 11) || (b >= 6 && b < 14) ? "Верно" : "Неверно";
+alert(resalt);*/
+
+let num = +prompt("Введите число");
+switch(num) {
+    case 1:
+        alert("Зима");
+     break;
+    case 2:
+        alert("Весна");
+     break;
+    case 3:
+        alert("Лето"); 
+    break;
+    case 4:
+        alert("Осень"); 
+}
